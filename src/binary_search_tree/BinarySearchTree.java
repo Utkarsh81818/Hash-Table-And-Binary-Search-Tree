22a4382fae8536157c3bd3ad6a64045b342d0c56
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-27 23:27:49 +0530

    Added: To create Binary Search Tree and add more values

--- src/binary_search_tree/BinarySearchTree.java
+++ src/binary_search_tree/BinarySearchTree.java
@@ -1,6 +1,6 @@
 package binary_search_tree;
 /**
- * Purpose  - To create Binary Search Tree
+ * Purpose  - To create Binary Search Tree and add more values
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-09-27
@@ -13,6 +13,16 @@ public class BinarySearchTree {
         myTree.add(56);
         myTree.add(30);
         myTree.add(70);
+        myTree.add(22);
+        myTree.add(40);
+        myTree.add(11);
+        myTree.add(3);
+        myTree.add(16);
+        myTree.add(60);
+        myTree.add(95);
+        myTree.add(65);
+        myTree.add(63);
+        myTree.add(67);
         myTree.print(myTree.root);
     }
 }
